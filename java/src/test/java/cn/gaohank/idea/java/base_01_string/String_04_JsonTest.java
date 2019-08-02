bfb80153faef6541aaddcab6bc29bf1630c78109
gaosongling <gaoslchn@163.com>
2019-08-02 15:03:50 +0800

    增加测试用例

---
+++ java/src/test/java/cn/gaohank/idea/java/base_01_string/String_04_JsonTest.java
@@ -0,0 +1,12 @@
+package cn.gaohank.idea.java.base_01_string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class String_04_JsonTest {
+    @Test
+    public void testNum() {
+        System.out.println(Double.valueOf("１６７"));
+    }
+}
