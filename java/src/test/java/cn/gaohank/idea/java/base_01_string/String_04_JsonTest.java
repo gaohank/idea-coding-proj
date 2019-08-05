5e80ea5df9e426314edbf71baf08c290ae8e6871
gaosongling <gaoslchn@163.com>
2019-08-05 13:53:49 +0800

    增加map的json测试

--- java/src/test/java/cn/gaohank/idea/java/base_01_string/String_04_JsonTest.java
+++ java/src/test/java/cn/gaohank/idea/java/base_01_string/String_04_JsonTest.java
@@ -1,12 +1,25 @@
 package cn.gaohank.idea.java.base_01_string;
 
+import com.google.gson.Gson;
+import lombok.Data;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.HashMap;
 
+@Data
 public class String_04_JsonTest {
+    private HashMap<String, String> stuMap;
+    private String name;
+
     @Test
     public void testNum() {
         System.out.println(Double.valueOf("１６７"));
+    }
+
+    @Test
+    public void testMap() {
+        String_04_JsonTest string_04_jsonTest = new String_04_JsonTest();
+        string_04_jsonTest.setStuMap(new HashMap<String, String>() {{ put("name", "hank"); put("age", "29"); }});
+        System.out.println(new Gson().toJson(string_04_jsonTest));
     }
 }
