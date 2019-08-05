d7875b4fd0a591760f35eeaf1fc49da40465f524
gaosongling <gaoslchn@163.com>
2019-08-05 17:23:54 +0800

    增加main方法

--- j2ee/j2ee-es/src/main/java/cn/gaohank/idea/j2ee/es/Application.java
+++ j2ee/j2ee-es/src/main/java/cn/gaohank/idea/j2ee/es/Application.java
@@ -1,4 +1,26 @@
 package cn.gaohank.idea.j2ee.es;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Slf4j
+@SpringBootApplication
 public class Application {
+    @PostConstruct
+    private void init() {
+        log.info("starting scheduler");
+    }
+
+    @PreDestroy
+    private void destroy() {
+        log.info("shutdown scheduler");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
