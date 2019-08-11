46e0a23242ba65a91170fc9385ce12bbc7e5ace1
gaosongling <gaosongling@xiaomi.com>
2019-08-11 16:28:56 +0800

    成功写入thriftSequenceFile

--- hadoop/common/src/main/scala/cn/gaohank/idea/hadoop/common/HdfsIO.scala
+++ hadoop/common/src/main/scala/cn/gaohank/idea/hadoop/common/HdfsIO.scala
@@ -1,10 +1,11 @@
 package cn.gaohank.idea.hadoop.common
 
-import java.io.{ByteArrayOutputStream, InputStream}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
 import org.apache.hadoop.io.compress.{CompressionCodec, DefaultCodec}
 import org.apache.hadoop.io.{BytesWritable, Writable}
 import org.apache.hadoop.mapred.{JobConf, SequenceFileOutputFormat}
+import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.thrift.TBase
 import org.apache.thrift.protocol.{TBinaryProtocol, TCompactProtocol}
@@ -14,9 +15,10 @@ import org.slf4j.{Logger, LoggerFactory}
 import scala.reflect.ClassTag
 
 object HdfsIO {
-  private val logger: Logger = LoggerFactory.getLogger(this.getClass.getSimpleName)
+  private val logger: Logger =
+    LoggerFactory.getLogger(this.getClass.getSimpleName)
 
-  def serCompactThrift[T <: TBase[_, _] : ClassTag](info: T): Array[Byte] = {
+  def serCompactThrift[T <: TBase[_, _]: ClassTag](info: T): Array[Byte] = {
     val out = new ByteArrayOutputStream()
     val transport = new TIOStreamTransport(out)
     val protocol = new TCompactProtocol(transport)
@@ -24,19 +26,26 @@ object HdfsIO {
     out.toByteArray
   }
 
-  def deCompactThrift[T <: TBase[_, _] : ClassTag](info: T, in: InputStream): T = {
-    val transport = new TIOStreamTransport(in)
+  def deCompactThrift[T <: TBase[_, _]: ClassTag](info: T, bytes: Array[Byte]): T = {
+    val transport = new TIOStreamTransport(new ByteArrayInputStream(bytes))
     val protocol = new TCompactProtocol(transport)
     info.read(protocol)
     info
   }
 
-  def serBinaryThrift[T <: TBase[_, _] : ClassTag](info: T): Array[Byte] = {
+  def serBinaryThrift[T <: TBase[_, _]: ClassTag](info: T): Array[Byte] = {
     val out = new ByteArrayOutputStream()
     val transport = new TIOStreamTransport(out)
     val protocol = new TBinaryProtocol(transport)
     info.write(protocol)
     out.toByteArray
+  }
+
+  def deBinaryThrift[T <: TBase[_, _]: ClassTag](info: T, bytes: Array[Byte]): T = {
+    val transport = new TIOStreamTransport(new ByteArrayInputStream(bytes))
+    val protocol = new TBinaryProtocol(transport)
+    info.read(protocol)
+    info
   }
 
   /**
@@ -68,6 +77,28 @@ object HdfsIO {
           jobConf,
           Some(codec)
         )
+    }
+  }
+
+  /**
+    * 为SparkConetxt配上thriftParquetFile方法，直接加载parquet文件为thrift对象RDD
+    */
+  implicit class SparkContextThriftFileWrapper[T <: TBase[_, _]: ClassTag](val sc: SparkContext) {
+    def thriftSequenceFile(path: String,
+                           thriftClass: Class[T],
+                           protocal: String = "compact"): RDD[T] = {
+      sc.sequenceFile[Array[Byte], Array[Byte]](path)
+        .mapPartitions { it =>
+          for ((_, v) <- it) yield {
+            val o = thriftClass.newInstance
+            protocal match {
+              case "compact" => deCompactThrift(o, v)
+              case "binary"  => deBinaryThrift(o, v)
+              case _ => new IllegalArgumentException(s"unsupported protocal: $protocal")
+            }
+            o
+          }
+        }
     }
   }
 }
