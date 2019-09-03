ab99434bdccaaf39686844e2cef13bf95fae940e
gaosongling <gaoslchn@163.com>
2019-09-03 09:22:49 +0800

    修改flink的测试

--- java/src/main/java/cn/gaohank/idea/java/base_14_flink/WindowWordCount.java
+++ java/src/main/java/cn/gaohank/idea/java/base_14_flink/WindowWordCount.java
@@ -5,7 +5,6 @@ import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.util.Collector;
 
 public class WindowWordCount {
     public static void main(String[] args) throws Exception {
@@ -14,7 +13,7 @@ public class WindowWordCount {
         final int port;
         final String host;
         port = 9008; // nc监听的tcp端口
-        host = "192.168.31.168"; // docker宿主机ip
+        host = "192.168.98.225"; // docker宿主机ip
 
         // get the execution environment
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -24,22 +23,14 @@ public class WindowWordCount {
 
         // parse the data, group it, window it, and aggregate the counts
         DataStream<WordWithCount> windowCounts = text
-                .flatMap(new FlatMapFunction<String, WordWithCount>() {
-                    @Override
-                    public void flatMap(String value, Collector<WordWithCount> out) {
-                        for (String word : value.split("\\s")) {
-                            out.collect(new WordWithCount(word, 1L));
-                        }
+                .flatMap((FlatMapFunction<String, WordWithCount>) (value, out) -> {
+                    for (String word : value.split("\\s")) {
+                        out.collect(new WordWithCount(word, 1L));
                     }
                 })
                 .keyBy("word")
                 .timeWindow(Time.seconds(5), Time.seconds(1))
-                .reduce(new ReduceFunction<WordWithCount>() {
-                    @Override
-                    public WordWithCount reduce(WordWithCount a, WordWithCount b) {
-                        return new WordWithCount(a.word, a.count + b.count);
-                    }
-                });
+                .reduce((ReduceFunction<WordWithCount>) (a, b) -> new WordWithCount(a.word, a.count + b.count));
 
         // print the results with a single thread, rather than in parallel
         windowCounts.print().setParallelism(1);
@@ -52,8 +43,6 @@ public class WindowWordCount {
 
         public String word;
         public long count;
-
-        public WordWithCount() {}
 
         public WordWithCount(String word, long count) {
             this.word = word;
